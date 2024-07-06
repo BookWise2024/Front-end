650ca896c2ea3671b98f6c0f85d06325bc15f9dd
db <emfprhs1579@gmail.com>
2024-07-06 13:29:11 +0900

    booklike finish

--- src/pages/BookLike/BookLike.jsx
+++ src/pages/BookLike/BookLike.jsx
@@ -7,16 +7,18 @@ import styles from './BookLike.module.css';
 import Layout from '../../Common/Layout/Layout.jsx';
 import getBookLikes from '../../API/BookLike/getBookLikes.js';
 
-const books = [
-  { title: '해리포터와 마법사의 돌', img: 'src/assets/img/book/book2.png' },
-  { title: '해리포터와 비밀의 방', img: 'src/assets/img/book/book2.png' },
-  { title: '해리포터와 아즈카반의 죄수', img: 'src/assets/img/book/book3.png' },
-  { title: '해리포터와 불의 잔', img: 'src/assets/img/book/book4.png' }
-];
+// const books = [
+//   { title: '해리포터와 마법사의 돌', img: 'src/assets/img/book/book2.png' },
+//   { title: '해리포터와 비밀의 방', img: 'src/assets/img/book/book2.png' },
+//   { title: '해리포터와 아즈카반의 죄수', img: 'src/assets/img/book/book3.png' },
+//   { title: '해리포터와 불의 잔', img: 'src/assets/img/book/book4.png' }
+// ];
 
 const BookLike = () => {
 
   const navigate = useNavigate();
+  // login token
+  const token = localStorage.getItem('accessToken');
   const [user, setUser] = useState(null);
   // 선호책 리스트
   const [prefer, setPrefer] = useState(null);
@@ -24,7 +26,6 @@ const BookLike = () => {
   // 로그인 여부 확인
   useEffect(() => {
     const login_check = async() => {
-      const token = localStorage.getItem('accessToken');
       if (token) {
         try {
           const response = await axios.get('http://43.203.74.198:8000/api/user', {
@@ -42,7 +43,6 @@ const BookLike = () => {
     }
 
     const preferList = async() => {
-      const token = localStorage.getItem('accessToken');
       try {
         const res = await axios.get('http://43.203.74.198:8000/api/wishlist', {
           headers: {
@@ -79,12 +79,12 @@ const BookLike = () => {
         <div className={styles.bookListContent}>
           <div className={styles.bookListHeader}>
             <div className={styles.title}>선호책 리스트</div>
-            <div className={styles.totalCount}>총 {books.length} 건</div>
+            <div className={styles.totalCount}>총 {prefer.count} 건</div>
           </div>
           <div className={styles.bookList}>
             {books.map((book, index) => (
               <div className={styles.bookItem} key={index}>
-                <img src={book.img} alt={book.title} className={styles.bookImage} />
+                <img src={prefer.coverUrl} className={styles.bookImage} />
               </div>
             ))}
           </div>
