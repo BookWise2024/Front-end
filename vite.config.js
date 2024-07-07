5987fee6ab3eee02b697c0d4c9174a810b2f9765
Kanon <sunny100487@naver.com>
2024-07-07 21:39:36 +0900

    fix: proxy api 수정

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,11 +25,30 @@ export default defineConfig({
       //   target: 'http://localhost:8080',
       //   changeOrigin: true,
       // }
-      '/api': {
-        target: 'https://www.aladin.co.kr',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/ttb/api')
-      }
+      // '/api': {
+      //   target: 'https://www.aladin.co.kr',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/api/, '/ttb/api', '')
+      // }
+    },
+    configureServer: ({ middlewares }) => {
+      middlewares.use('/api', (req, res, next) => {
+        if (req.url.startsWith('/api/loantItemSrch')) {
+          createProxyMiddleware({
+            target: 'http://data4library.kr',
+            changeOrigin: true,
+            pathRewrite: { '^/api': '' },
+          })(req, res, next);
+        } else if (req.url.startsWith('/api/ItemSearch.aspx')) {
+          createProxyMiddleware({
+            target: 'https://www.aladin.co.kr',
+            changeOrigin: true,
+            pathRewrite: { '^/api': '' },
+          })(req, res, next);
+        } else {
+          next();
+        }
+      });
     },
   }
 })
