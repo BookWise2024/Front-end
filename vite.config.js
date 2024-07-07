a703ea39ad19d537c816db982b2898336ce17447
Kanon <sunny100487@naver.com>
2024-07-07 21:45:51 +0900

    fix: 프록시 복구

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import react from '@vitejs/plugin-react';
+// import { createProxyMiddleware } from 'http-proxy-middleware';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,30 +25,12 @@ export default defineConfig({
       //   target: 'http://localhost:8080',
       //   changeOrigin: true,
       // }
-      // '/api': {
-      //   target: 'https://www.aladin.co.kr',
-      //   changeOrigin: true,
-      //   rewrite: (path) => path.replace(/^\/api/, '/ttb/api', '')
-      // }
+      '/api': {
+        target: 'https://www.aladin.co.kr',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/ttb/api', '')
+      }
     },
-    configureServer: ({ middlewares }) => {
-      middlewares.use('/api', (req, res, next) => {
-        if (req.url.startsWith('/api/loantItemSrch')) {
-          createProxyMiddleware({
-            target: 'http://data4library.kr',
-            changeOrigin: true,
-            pathRewrite: { '^/api': '' },
-          })(req, res, next);
-        } else if (req.url.startsWith('/api/ItemSearch.aspx')) {
-          createProxyMiddleware({
-            target: 'https://www.aladin.co.kr',
-            changeOrigin: true,
-            pathRewrite: { '^/api': '' },
-          })(req, res, next);
-        } else {
-          next();
-        }
-      });
-    },
+    
   }
 })
